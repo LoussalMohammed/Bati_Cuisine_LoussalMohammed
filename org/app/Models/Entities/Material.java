e18a44cf61cf1a6d02554d4bcd6d25d5d2115865
loussalmohammed <hplaptopsimo@gmail.com>
2024-09-20 17:22:12 +0100

    Add Material Model Enitity

--- org/app/Models/Entities/Material.java
+++ org/app/Models/Entities/Material.java
@@ -1,4 +1,50 @@
 package org.app.Models.Entities;
 
 public class Material {
+
+    private double quantite;
+
+    private double coutTransport;
+
+    private double coefficientQualte;
+    private double coutUnitaire;
+
+    public Material(double quantite, double coutTransport, double coefficientQualte, double coutUnitaire) {
+        this.quantite = quantite;
+        this.coutTransport = coutTransport;
+        this.coefficientQualte = coefficientQualte;
+        this.coutUnitaire = coutUnitaire;
+    }
+
+    public double getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(double quantite) {
+        this.quantite = quantite;
+    }
+
+    public double setCoutTransport() {
+        return coutTransport;
+    }
+
+    public void setCoutTransport(double coutTransport) {
+        this.coutTransport = coutTransport;
+    }
+
+    public double getCoefficientQualte() {
+        return coefficientQualte;
+    }
+
+    public void setCoefficientQualte(double coefficientQualte) {
+        this.coefficientQualte = coefficientQualte;
+    }
+
+    public double getCoutUnitaire() {
+        return coutUnitaire;
+    }
+
+    public void setCoutUnitaire(double coutUnitaire) {
+        this.coutUnitaire = coutUnitaire;
+    }
 }

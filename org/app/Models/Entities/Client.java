d50c760df968dcf94d5cccf7b9101ce62289c4ff
loussalmohammed <hplaptopsimo@gmail.com>
2024-09-20 17:21:15 +0100

    Add Client Model Enitity

--- org/app/Models/Entities/Client.java
+++ org/app/Models/Entities/Client.java
@@ -8,5 +8,51 @@ public class Client {
     private boolean estProfessionnel;
     private double remise;
 
+    public Client(String nom, String address, String phone, boolean estProfessionnel, double remise) {
+        this.nom = nom;
+        this.address = address;
+        this.phone = phone;
+        this.estProfessionnel = estProfessionnel;
+        this.remise = remise;
+    }
 
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String setPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean getEstProfessionnel() {
+        return estProfessionnel;
+    }
+
+    public void setEstProfessionnel(boolean estProfessionnel) {
+        this.estProfessionnel = estProfessionnel;
+    }
+
+    public double getRemise() {
+        return remise;
+    }
+
+    public void setRemise(double remise) {
+        this.remise = remise;
+    }
 }

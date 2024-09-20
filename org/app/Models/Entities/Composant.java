baf26bb0af71738055aeeec707007eeb09ae088a
loussalmohammed <hplaptopsimo@gmail.com>
2024-09-20 17:21:48 +0100

    Add Composantt Model Enitity

--- org/app/Models/Entities/Composant.java
+++ org/app/Models/Entities/Composant.java
@@ -1,5 +1,32 @@
 package org.app.Models.Entities;
 
-public class Composant {
+import org.app.Models.Enums.TypeComposant;
 
+public abstract class Composant {
+
+    protected String nom;
+    protected TypeComposant typeComposant;
+
+    public Composant(String nom, TypeComposant typeComposant) {
+        this.nom = nom;
+        this.typeComposant = typeComposant;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public TypeComposant getTypeComposant() {
+        return typeComposant;
+    }
+
+    public void setTypeComposant(TypeComposant typeComposant) {
+        this.typeComposant = typeComposant;
+    }
+
+    public abstract double calculeCout();
 }
